9a97144ea0f2899b3da47c746682876c06fb5016
agentlincx <lincxace@gmail.com>
2024-09-06 08:06:37 +0100

    update

--- dash.php
+++ dash.php
@@ -34,9 +34,18 @@ require "val.php";
         if (isset($_GET["no"])) {
             $a = $_GET["no"];
             $r = "select * from track where num='$a' order by id desc";
+            $rr = "select * from ufrom where num='$a'";
+            $rq = "select * from quote where num='$a'";
+
             $b = mysqli_query($con, $r);
             if ($b) {
-                if (mysqli_num_rows($b) > 0) { ?>
+                if (mysqli_num_rows($b) > 0) {
+
+                    $bb = mysqli_query($con, $rr);
+                    $bq = mysqli_query($con, $rq);
+                    $dd = mysqli_fetch_assoc($bb);
+                    $dq = mysqli_fetch_assoc($bq);
+                    ?>
 
         <div id="tk">
             <p class="a">
@@ -45,7 +54,80 @@ require "val.php";
             <p class="b">This shipment is handled by Icarus Logistics</p>
         </div>
         
-        <div id="addq" class="sub" onclick="addq('<?php echo $_GET["no"];?>')">
+        
+  <div id="trtab">
+      <div>
+          <p>Sender Details</p>
+          <table>
+              <tr>
+                  <th>Sender Name</th>
+                  <th>Address</th>
+                  <th>Email</th>
+              </tr>
+              <tr>
+                  <td><?php echo $dd["name"];?></td>
+                  <td><?php echo $dd["address"];?></td>
+                  <td><?php echo $dd["email"];?></td>
+              </tr>
+          </table>
+      </div>
+      
+      <div>
+          <p>Consignment's Details</p>
+          <table>
+              <tr>
+                  <th>Tracking Number</th>
+                  <th>Package Weight</th>
+                  <th>Status</th>
+                  
+              </tr>
+              <tr>
+                  <td><?php echo $dq["num"];?></td>
+                  <td><?php echo $dq["weight"]."kg";?></td>
+                  <td><?php
+                  $cv = $dq["val"];
+                  if ($cv == 0){
+                      echo "Order Processing";
+                  } else if ($cv == 100) {
+                      echo "Order Arrived";
+                  } else {
+                      echo "In-transit";
+                  }
+                  
+                  ?></td>
+              </tr>
+          </table>
+      </div>
+      
+      <div>
+          <p>Order Details</p>
+          <table border="0">
+              <tr>
+                  <th>Order Location</th>
+                  <th>Order Date</th>
+                  <th>Arrival Day</th>
+                  <th>Destination Country</th>
+              </tr>
+              <tr>
+                  <td><?php
+                  echo $dq["from"];
+                  ?></td>
+                  <td><?php
+                  echo $dq["date"];
+                  ?></td>
+                  <td><?php
+                            $est = strtotime(($dq["date"])) + (60*60*24*4);
+                            echo date("Y-m-d",$est); ?></td>
+                  <td><?php
+                  echo $dq["to"];
+                  ?></td>
+              </tr>
+          </table>
+      </div>
+  </div>
+  
+        
+        <div id="addq" class="sub" onclick="addq('<?php echo $_GET["no"]; ?>')">
             New&nbsp; <i class="fas fa-add"></i>
         </div>
 
@@ -53,8 +135,12 @@ require "val.php";
             <?php while ($d = mysqli_fetch_assoc($b)) { ?>
             <div class="rec" 
             <?php if ($d["desc"] == "Shipment order has been created") {
-                    } else {?>
-            onclick="onb(<?php echo $d["id"]; ?>,'<?php echo $_GET["no"];?>')" <?php } ?>>
+            } else {
+                 ?>
+            onclick="onb(<?php echo $d["id"]; ?>,'<?php echo $_GET[
+    "no"
+]; ?>')" <?php
+            } ?>>
                 <i class="fas fa-circle"></i>
                 <div class="a"><?php echo $d["desc"]; ?></div>
                 <div class="b">
@@ -85,13 +171,15 @@ require "val.php";
         <script src="main.js"></script>
         <script type="text/javascript" charset="utf-8">
             
-            var ty = '<?php echo loc($a);?>';
+            var ty = '<?php echo loc($a); ?>';
             loc(ty);
         </script>
         
         
 
-        <?php } else { ?>
+        <?php
+                } else {
+                     ?>
 
         <div id="err">
             <i class="fas fa-exclamation-triangle fa-2x"></i>
@@ -104,7 +192,8 @@ require "val.php";
             </div>
         </div>
 
-        <?php }
+        <?php
+                }
             }
         } else {
         }
@@ -131,7 +220,9 @@ require "val.php";
                         </div>
                         <div>
                             <label for="">Tracking %</label>
-                            <input required type="number" max="100" name="descd" value="<?php echo loc($a);?>" id="desc" />
+                            <input required type="number" max="100" name="descd" value="<?php echo loc(
+                                $a
+                            ); ?>" id="desc" />
                         </div>
                     </div>
                     <input type="submit" name="" class="sub" value="Update" />
@@ -157,7 +248,9 @@ require "val.php";
                         </div>
                         <div>
                             <label for="">Tracking %</label>
-                            <input required type="text" max="100" name="descd" value="<?php echo loc($a);?>" id="desc" />
+                            <input required type="text" max="100" name="descd" value="<?php echo loc(
+                                $a
+                            ); ?>" id="desc" />
                         </div>
                     </div>
                     <input type="submit" name="" class="sub" value="Add" />
--- track.php
+++ track.php
@@ -21,15 +21,88 @@
       $a = $_GET["no"];
 
       $r = "select * from track where num='$a' order by id desc";
+      $rr = "select * from ufrom where num='$a'";
+      $rq = "select * from quote where num='$a'";
 
       $b = mysqli_query($con, $r);
 
       if ($b) {
-          if (mysqli_num_rows($b) > 0) { ?>
+          if (mysqli_num_rows($b) > 0) {
+
+              $bb = mysqli_query($con, $rr);
+              $bq = mysqli_query($con, $rq);
+              $dd = mysqli_fetch_assoc($bb);
+              $dq = mysqli_fetch_assoc($bq);
+              ?>
   
   <div id="tk">
     <p class="a">Tracking Code : <span class="id"><?php echo $a; ?></span></p>
     <p class="b">This shipment is handled by Icarus Logistics</p>
+  </div>
+  
+  <div id="trtab">
+      <div>
+          <p>Sender Details</p>
+          <table>
+              <tr>
+                  <th>Sender Name</th>
+                  <th>Address</th>
+                  <th>Email</th>
+              </tr>
+              <tr>
+                  <td><?php echo $dd["name"]; ?></td>
+                  <td><?php echo $dd["address"]; ?></td>
+                  <td><?php echo $dd["email"]; ?></td>
+              </tr>
+          </table>
+      </div>
+      
+      <div>
+          <p>Consignment's Details</p>
+          <table>
+              <tr>
+                  <th>Tracking Number</th>
+                  <th>Package Weight</th>
+                  <th>Status</th>
+                  
+              </tr>
+              <tr>
+                  <td><?php echo $dq["num"]; ?></td>
+                  <td><?php echo $dq["weight"] . "kg"; ?></td>
+                  <td><?php
+                  $cv = $dq["val"];
+                  if ($cv == 0) {
+                      echo "Order Processing";
+                  } elseif ($cv == 100) {
+                      echo "Order Arrived";
+                  } else {
+                      echo "In-transit";
+                  }
+                  ?></td>
+              </tr>
+          </table>
+      </div>
+      
+      <div>
+          <p>Order Details</p>
+          <table border="0">
+              <tr>
+                  <th>Order Location</th>
+                  <th>Order Date</th>
+                  <th>Arrival Day</th>
+                  <th>Destination Country</th>
+              </tr>
+              <tr>
+                  <td><?php echo $dq["from"]; ?></td>
+                  <td><?php echo $dq["date"]; ?></td>
+                  <td><?php
+                  $est = strtotime($dq["date"]) + 60 * 60 * 24 * 4;
+                  echo date("Y-m-d", $est);
+                  ?></td>
+                  <td><?php echo $dq["to"]; ?></td>
+              </tr>
+          </table>
+      </div>
   </div>
   
   <div id="lk">
@@ -76,7 +149,9 @@
         
         
   
-  <?php } else { ?>
+  <?php
+          } else {
+               ?>
   
   <div id="err">
     <i class="fas fa-exclamation-triangle fa-2x"></i>
@@ -86,7 +161,8 @@
     </div>
   </div>
   
-  <?php }
+  <?php
+          }
       }
   } else {
   }
